e037a385ee7d2b33a57b14b6a486617553e4185a
Shaun <shaunmathew123@gmail.com>
2023-03-11 22:36:53 -0500

    some misc changes to snip and treesitter

--- lua/plugins/LuaSnip.lua
+++ lua/plugins/LuaSnip.lua
@@ -1,17 +1,16 @@
 return {
-  "L3MON4D3/LuaSnip",
-  event = "InsertEnter",
-  dependencies = {
-    { "rafamadriz/friendly-snippets", event = "InsertEnter" }
-  },
-  config = function()
-    require("luasnip").config.set_config({
-      history = true,
-      updateevents = "TextChanged,TextChangedI"
-    })
+	"L3MON4D3/LuaSnip",
+	event = "InsertEnter",
+	dependencies = {
+		{ "rafamadriz/friendly-snippets", event = "InsertEnter" },
+	},
+	config = function()
+		require("luasnip").config.set_config({
+			history = true,
+			updateevents = "TextChanged,TextChangedI",
+		})
 
-    require("luasnip.loaders.from_vscode").lazy_load { paths = vim.g.luasnippets_path or "" }
-    require("luasnip.loaders.from_vscode").lazy_load()
-    
-  end
+		require("luasnip.loaders.from_vscode").lazy_load({ paths = vim.g.luasnippets_path or "" })
+		require("luasnip.loaders.from_vscode").lazy_load()
+	end,
 }
